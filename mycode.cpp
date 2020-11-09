547397b8bbaf99c8be6c75cd060e3fb1b4281d6a
Mohamed Babiker <onepace360@gmail.com>
2020-11-09 14:07:19 +0200

    Added Arrays To be able to entered 10 students information

--- mycode.cpp
+++ mycode.cpp
@@ -8,62 +8,83 @@
 using namespace std ;
 int main()
 {  
-	string  section , dgree , Class ,name , exit; // Here we Defined the variable as String
-	 // here the user may enter his full name 
-	int score , ID ;  // Define score and ID as integer
+	char section [10] , dgree[10] ; // Here we Defined the variable as String
+	int everage[10] , score = 0 , ID [10];  // Define score and ID as integer
+	string exit , student [10] ,  Class [10]; //Array Have a number of Students and section class
+	int material [8] ; //Array of number of the matirals
+	string materials [8] = {"English : ","Math : ","C++ Programming : ","History : ","Physics : ","Engineering : ","Economic : ","Medicine : "};
 	
 	do{
 	
-	cout<<"Enter Your Name : "; // Dislapy A message on screen and wait for user to input his Name
-	cin>>name ;  
-	cout<<"Enter Your ID : "; cin >> ID ; // ask user to Enter his ID
-	cout<<"Select 'A'  Control Class  "<<endl<<"Select 'T' Communication class "<< endl<< "Select 'C' computer Engneering "<<endl;
-	cin >> section ; // For input the Section
-	if (section == "A") // if condition statment
+	for ( int i = 0; i < 10; i++) // for loop to Enter information of Students
 	{
-		Class = " Control"; // here if user input A class will be Control
-	}
-	else if (section == "T") {
-		Class = "Communication"; // here if user input something else class will be Communication
-	}
-	else if(section == "C"){
-		Class = "computer Engneering" ;
-	}
-	else {
-		cout<<"sorry! Department not Found ! try again....."<<endl;
-	}
-	
-	cout<<"Enter The student Score : "; cin >> score ; // here the user will input his Score
-	switch(score) // Switch statement to check if score between 90 and 100
+	cout<<"Enter Student Name : "; //Students name
+	    cin >> student[i] ;  
+	cout<<"Enter Student ID : "; // ask  to Enter Student ID
+		cin >> ID[i] ;
+	cout<<"Select : A or T or C Class .....";
+		cin >> section[i] ; // For input the Section
+	if (section[i] == 'A')
+	{ 
+		Class[i] = "Control"; //  A class will be Control
+	} 
+	else if (section[i] == 'T')
 	{
-	case 90 ... 100 :
-	 dgree = "A";
-	 break ;
-	case 80 ... 89 :
-	 dgree = "B"; // and it will check the score to display the right Dgree	
-	 break ;
-	case 70 ... 79 :
-	 dgree = "C";
-	 break ;
-	case 60 ... 69 :
-	 dgree = "D";
-	 break ;
-	case 50 ... 59 : 
-	dgree = "F";
-	break ;
-	default :
-	cout << "Invalid Score" ;
+		Class[i] = "Communication"; // here if user input something else class will be Communication
 	}
-	cout<< "\n"<<"=====Student Information====="<<"\n";
-	// Finaly This Line Will Display all the information that user Entered before
-	cout << "Name : "<< name << "\n"<<"ID : "<< ID << "\n"<<"Department : "<< Class << "\n"<<"Dgree : "<< dgree << endl ;
+	else if(section[i] == 'C')
+	{
+		Class [i] = "computer Engneering" ;
+	}
+	cout<<" Enter Score Of Material"<<endl;
+		for ( int j = 0; j < 8; j++) //for loop to Entered score of 8 Materails 
+	{
+		cout<<materials[j];
+		cin>>material[j];
+	}
+		for(int k = 0 ; k<8;k++)
+		{
+		score += material[k]; //Score Equal the Sum of all Materials
+		}
+		for (int i = 0; i < 10; i++)
+		{
+		everage[i] = score/8 ; // to calculate the everage of Materails
+		}
+		switch(everage[i]) // Switch statement to check if score between 90 and 100
+		{
+		case 90 ... 100 :
+		dgree[i] = 'A';
+		break ;
+		case 80 ... 89 :
+		dgree[i] = 'B'; 
+		break ;
+		case 70 ...79 :
+		dgree [i]= 'C';
+		break ;
+		case 60 ... 69 :
+		dgree [i]= 'D';
+		break ;
+		case 50 ... 59 : 
+		dgree [i]= 'F';
+		break ;
+		default :
+		dgree [i]= 'F' ;
+	    }
 
+	}	
+    // Finaly This Line Will Display all the information that  Entered before
+	cout<< "\n\n"<<"=====Student Information====="<<"\n\n";
+	
+	for (int i = 1; i <= 11; i++)
+	{
+	    cout << "Student :"<<i<<"  Name :"<< student[i] << " "<<"  ID: "<<ID[i]<<"  "<<"Department: "<< Class [i]<< " "<<"   Everage: "<<everage[i]<<" "<<"Dgree : "<< dgree[i] << endl ;
+	}
+	 //Asking if stil have a 10 new Student to Entered their Information
 	cout<<"Type 'Exit' to stop ! or Type 'No' to Continue... "<<endl;
 	cin >> exit ;
 
 	}while ( exit != "Exit" );
-	cout<<" \n";
 
 	return 0;
-	
+
 }
